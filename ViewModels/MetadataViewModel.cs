bebcdddec2ba4fe08fde2b5ef0b3c1ba3d60ddca
AlexEW <alexelliswilson@gmail.com>
2024-05-15 00:06:39 +0100

    Add project files.

---
+++ ViewModels/MetadataViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlexPortfolio.ViewModels
+{
+    public class MetadataViewModel
+    {
+        public string? Title { get; set; }
+        public string? MetaDescription { get; set; }
+        public bool NoIndex { get; set; }
+    }
+}
