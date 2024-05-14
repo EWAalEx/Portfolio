b3a681d7e849cf1dd73f721b91eac2b82dc524bb
AlexEW <alexelliswilson@gmail.com>
2024-05-15 00:39:17 +0100

    removed ranom files from old project

--- ViewModels/MetadataViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AlexPortfolio.ViewModels
-{
-    public class MetadataViewModel
-    {
-        public string? Title { get; set; }
-        public string? MetaDescription { get; set; }
-        public bool NoIndex { get; set; }
-    }
-}
